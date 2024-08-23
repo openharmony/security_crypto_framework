04855bce1473a7363f00174a5d8a8ec3210b7fa5
lcc <lichaochen@huawei.com>
2024-08-23 22:54:17 +0800

    修改白盒检视issue
    Signed-off-by: lcc <lichaochen@huawei.com>
    Signed-off-by: hhhFun <fanghaojie@huawei.com>

--- frameworks/js/napi/crypto/src/napi_sign.cpp
+++ frameworks/js/napi/crypto/src/napi_sign.cpp
@@ -170,7 +170,7 @@ static bool BuildSignJsInitCtx(napi_env env, napi_callback_info info, SignInitCt
     napi_value thisVar = nullptr;
     size_t expectedArgc = PARAMS_NUM_TWO;
     size_t argc = expectedArgc;
-    napi_value argv[PARAMS_NUM_TWO] = { nullptr, nullptr };
+    napi_value argv[PARAMS_NUM_TWO] = { nullptr };
     napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr);
     if (argc != expectedArgc && argc != expectedArgc - 1) {
         LOGE("wrong argument num. require %zu or %zu arguments. [Argc]: %zu!", expectedArgc - 1, expectedArgc, argc);
@@ -220,7 +220,7 @@ static bool BuildSignJsUpdateCtx(napi_env env, napi_callback_info info, SignUpda
     napi_value thisVar = nullptr;
     size_t expectedArgc = PARAMS_NUM_TWO;
     size_t argc = expectedArgc;
-    napi_value argv[PARAMS_NUM_TWO] = { nullptr, nullptr };
+    napi_value argv[PARAMS_NUM_TWO] = { nullptr };
     napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr);
     if ((argc != expectedArgc) && (argc != expectedArgc - 1)) {
         LOGE("wrong argument num. require %zu or %zu arguments. [Argc]: %zu!", expectedArgc - 1, expectedArgc, argc);
@@ -263,7 +263,7 @@ static bool BuildSignJsDoFinalCtx(napi_env env, napi_callback_info info, SignDoF
     napi_value thisVar = nullptr;
     size_t expectedArgc = PARAMS_NUM_TWO;
     size_t argc = expectedArgc;
-    napi_value argv[PARAMS_NUM_TWO] = { nullptr, nullptr };
+    napi_value argv[PARAMS_NUM_TWO] = { nullptr };
     napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr);
     if ((argc != expectedArgc) && (argc != expectedArgc - 1)) {
         LOGE("wrong argument num. require %zu or %zu arguments. [Argc]: %zu!", expectedArgc - 1, expectedArgc, argc);

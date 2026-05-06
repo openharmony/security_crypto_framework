72575ea9a98867c2e0ae01e5bdae47147764ee39
zxz*3 <zhangxiaozan1@huawei.com>
2026-05-06 11:27:21 +0800

    算法库新增ML-KEM算法封装与解封装
    Signed-off-by: zxz*3 <zhangxiaozan1@huawei.com>

---
+++ frameworks/js/ani/src/ani_kem.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2026-2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ani_kem.h"
+
+namespace {
+using namespace ANI::CryptoFramework;
+} // namespace
+
+namespace ANI::CryptoFramework {
+KemImpl::KemImpl() {}
+
+KemImpl::KemImpl(HcfKem *kem) : kem_(kem) {}
+
+KemImpl::~KemImpl()
+{
+    HcfObjDestroy(this->kem_);
+    this->kem_ = nullptr;
+}
+
+KemEncapResult KemImpl::EncapsulateSync(weak::PubKey pubKey, OptDataBlob const& ikme)
+{
+    if (this->kem_ == nullptr) {
+        ANI_LOGE_THROW(HCF_ERR_ANI, "kem obj is nullptr!");
+        return {};
+    }
+    HcfPubKey *hcfPubKey = reinterpret_cast<HcfPubKey *>(pubKey->GetPubKeyObj());
+    HcfBlob ikmeBlob = {};
+    HcfBlob *ikmePtr = nullptr;
+    if (ikme.get_tag() == OptDataBlob::tag_t::DATABLOB) {
+        ArrayU8ToDataBlob(ikme.get_DATABLOB_ref().data, ikmeBlob);
+        ikmePtr = &ikmeBlob;
+    }
+    HcfBlob sharedSecret = {};
+    HcfBlob wrappedKey = {};
+    HcfResult res = this->kem_->encapsulate(this->kem_, hcfPubKey, ikmePtr, &sharedSecret, &wrappedKey);
+    if (res != HCF_SUCCESS) {
+        ANI_LOGE_THROW(res, "kem encapsulate failed.");
+        return {};
+    }
+    array<uint8_t> secretData = {};
+    array<uint8_t> wrappedData = {};
+    DataBlobToArrayU8(sharedSecret, secretData);
+    DataBlobToArrayU8(wrappedKey, wrappedData);
+    HcfBlobDataClearAndFree(&sharedSecret);
+    HcfBlobDataClearAndFree(&wrappedKey);
+    return { secretData, wrappedData };
+}
+
+DataBlob KemImpl::DecapsulateSync(weak::PriKey priKey, DataBlob const& wrappedKey)
+{
+    if (this->kem_ == nullptr) {
+        ANI_LOGE_THROW(HCF_ERR_ANI, "kem obj is nullptr!");
+        return {};
+    }
+    HcfPriKey *hcfPriKey = reinterpret_cast<HcfPriKey *>(priKey->GetPriKeyObj());
+    HcfBlob wrappedKeyBlob = {};
+    ArrayU8ToDataBlob(wrappedKey.data, wrappedKeyBlob);
+    HcfBlob sharedSecret = {};
+    HcfResult res = this->kem_->decapsulate(this->kem_, hcfPriKey, &wrappedKeyBlob, &sharedSecret);
+    if (res != HCF_SUCCESS) {
+        ANI_LOGE_THROW(res, "kem decapsulate failed.");
+        return {};
+    }
+    array<uint8_t> secretData = {};
+    DataBlobToArrayU8(sharedSecret, secretData);
+    HcfBlobDataClearAndFree(&sharedSecret);
+    return { secretData };
+}
+
+static const char *GetKemAlgoNameById(KemAlgNameId algId)
+{
+    HcfKemAlgNameId id = static_cast<HcfKemAlgNameId>(algId.get_value());
+    switch (id) {
+        case ML_KEM_512:
+            return "ML-KEM512";
+        case ML_KEM_768:
+            return "ML-KEM768";
+        case ML_KEM_1024:
+            return "ML-KEM1024";
+        default:
+            return nullptr;
+    }
+}
+
+Kem CreateKem(KemAlgNameId algNameId)
+{
+    const char *algName = GetKemAlgoNameById(algNameId);
+    if (algName == nullptr) {
+        ANI_LOGE_THROW(HCF_ERR_PARAMETER_CHECK_FAILED, "Unsupported kem alg id.");
+        return make_holder<KemImpl, Kem>();
+    }
+    HcfKem *kem = nullptr;
+    HcfResult res = HcfKemCreate(algName, &kem);
+    if (res != HCF_SUCCESS) {
+        ANI_LOGE_THROW(res, "create kem obj failed.");
+        return make_holder<KemImpl, Kem>();
+    }
+    return make_holder<KemImpl, Kem>(kem);
+}
+} // namespace ANI::CryptoFramework
+
+// Since these macros are auto-generate, lint will cause false positive.
+// NOLINTBEGIN
+TH_EXPORT_CPP_API_CreateKem(ANI::CryptoFramework::CreateKem);
+// NOLINTEND

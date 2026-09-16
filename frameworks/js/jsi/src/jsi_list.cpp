bd0e58b7a58092e2a0bde747dd293e328f1619ee
pengjinhui <pengjinhui4@h-partners.com>
2026-09-16 16:36:06 +0800

    fixing:add comment for ObjList::listNode first-member constraint
    Signed-off-by: pengjinhui <pengjinhui4@h-partners.com>

--- frameworks/js/jsi/src/jsi_list.cpp
+++ frameworks/js/jsi/src/jsi_list.cpp
@@ -26,6 +26,7 @@
  * head:      ListNode* Head of the linked list
  * type:      Business structure type (ObjList)
  * member:    Name of the embedded linked list member (listNode)
+ * Relies on ObjList::listNode being the first member for safe empty-list termination. 
  */
 #define LIST_FOR_EACH_ENTRY_SAFE(item, itemNext, head, type, member) /* NOLINT(G.PRE.02-CPP)*/ \
     for ((item) = CRYPTO_CONTAINER_OF(((head)->next), type, member),              \
@@ -144,7 +145,6 @@ void ListDestroy(LiteAlgType type)
         ListDelete(&(obj->listNode));
         HcfObjDestroy(reinterpret_cast<void *>(tempAddr));
         HcfFree(obj);
-        obj = nullptr;
     }
 }
 
